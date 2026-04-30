e503df3fa4f184fd36cb3706a64fbdc9f13de6b5
arthur <arthursantos0017@gmail.com>
2026-04-30 13:07:58 -0300

    exs. propostos atualizados em partes section 6

---
+++ ExerciciosSection06/src/partedois/ExProposto01.java
@@ -0,0 +1,10 @@
+/*
+Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o
+X, se for o caso.
+*/
+
+package partedois;
+
+public class ExProposto01 {
+
+}
