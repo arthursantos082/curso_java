e67aa4925999ec3c50868e20bbc8f742a0cdfd22
arthur <arthursantos0017@gmail.com>
2026-05-01 11:34:36 -0300

    ex proposto 5 section 06 pt2

---
+++ ExerciciosSection06/src/partedois/ExProposto05.java
@@ -0,0 +1,25 @@
+/*
+Ler um valor N. Calcular e escrever seu respectivo fatorial. Fatorial de N = N * (N-1) * (N-2) * (N-3) * ... * 1.
+Lembrando que, por definição, fatorial de 0 é 1.
+*/
+
+package partedois;
+
+import java.util.Scanner;
+
+public class ExProposto05 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        int fatorial = 1;
+
+        for (int i = 1; i <= n; i++) {
+            fatorial *= i;
+        }
+
+        System.out.println(fatorial);
+
+        input.close();
+    }
+}
