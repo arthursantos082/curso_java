2ccac79b5d2818bdd1f05eca1201481258217587
arthur <arthursantos0017@gmail.com>
2026-05-01 10:15:15 -0300

    ex proposto 03 section 06 pt2

---
+++ ExerciciosSection06/src/partedois/ExProposto03.java
@@ -0,0 +1,33 @@
+/*
+Leia 1 valor inteiro N, que representa o número de casos de teste que vem a seguir. Cada caso de teste consiste
+de 3 valores reais, cada um deles com uma casa decimal. Apresente a média ponderada para cada um destes
+conjuntos de 3 valores, sendo que o primeiro valor tem peso 2, o segundo valor tem peso 3 e o terceiro valor tem
+peso 5.
+*/
+
+package partedois;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ExProposto03 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in).useLocale(Locale.US);
+        Locale.setDefault(Locale.US);
+
+        int n = input.nextInt();
+        double mediaPond;
+
+
+        for (int i = 0; i < (n); i++) {
+            double x1 = input.nextDouble();
+            double x2 = input.nextDouble();
+            double x3 = input.nextDouble();
+            mediaPond = (x1*2.0 + x2*3.0 + x3*5.0) / 10;
+            System.out.printf("%.1f\n", mediaPond);
+        }
+
+
+        input.close();
+    }
+}
