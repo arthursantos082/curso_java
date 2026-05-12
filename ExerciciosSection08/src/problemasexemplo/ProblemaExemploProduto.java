d1ad6e88765e2c9d773cc7cd351c0107c6eca1d3
arthur <arthursantos0017@gmail.com>
2026-05-12 16:23:46 -0300

    Section 08 - Começando a resolver um segundo problema utilizando orientação a objetos

---
+++ ExerciciosSection08/src/problemasexemplo/ProblemaExemploProduto.java
@@ -0,0 +1,28 @@
+// comecando a resolver um segundo problema usando orientacao a objetos
+
+package problemasexemplo;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ProblemaExemploProduto {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in).useLocale(Locale.US);
+        Locale.setDefault(Locale.US);
+
+        Produto product = new Produto();
+
+        System.out.println("Enter product data: ");
+        System.out.println("Name: ");
+        product.name = input.nextLine();
+
+        System.out.println("Price: ");
+        product.price = input.nextDouble();
+
+        System.out.println("Quantity in stock: ");
+        product.quantity = input.nextInt();
+
+        input.close();
+    }
+
+}
