ebc6b4088a85f66435247d5c2e21229e48654cba
arthur <arthursantos0017@gmail.com>
2026-05-21 11:31:50 -0300

    Section 08 - Ex. Proposto 01

---
+++ ExerciciosSection08/src/application/ExProposto01Retangulo.java
@@ -0,0 +1,29 @@
+/*
+ ler os valores da largura e altura de um retangulo
+ mostrar na tela o valor de sua area, perimetro e diagonal
+ usar uma classe
+*/
+
+package application;
+
+import entities.Retangulo;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ExProposto01Retangulo {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in).useLocale(Locale.US);
+        Locale.setDefault(Locale.US);
+
+        Retangulo retangulo =  new Retangulo();
+
+        System.out.println("Informe a largura e a altura do retangulo: ");
+        retangulo.l = input.nextDouble();
+        retangulo.h = input.nextDouble();
+
+        System.out.println(retangulo);
+
+        input.close();
+    }
+}
---
+++ ExerciciosSection08/src/entities/Retangulo.java
@@ -0,0 +1,29 @@
+package entities;
+
+public class Retangulo {
+    public double l;
+    public double h;
+
+    public double area(){
+        return l * h;
+    }
+
+    public double perimetro(){
+        return 2 * (l + h);
+    }
+
+    public double diagonal(){
+        return Math.sqrt((l * l) + (h * h));
+    }
+
+    public String toString(){
+        return "AREA = " +
+                String.format("%.2f", area()) +
+                "\n" +
+                "PERIMETRO = " +
+                String.format("%.2f", perimetro()) +
+                "\n" +
+                "DIAGONAL = " +
+                String.format("%.2f", diagonal()) ;
+    }
+}
