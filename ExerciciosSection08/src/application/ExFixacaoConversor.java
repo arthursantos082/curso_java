20b3563564b49a271dd8502ec8c812bdd6fd66f5
arthur <arthursantos0017@gmail.com>
2026-05-22 12:59:53 -0300

    Section 08 - Ex. Fixação Realizado

--- ExerciciosSection08/src/application/ExFixacaoConversor.java
+++ ExerciciosSection08/src/application/ExFixacaoConversor.java
@@ -1,7 +1,26 @@
+// ler a cotacao do dolar, e depois o valor em dolares a ser comprado em reais
+// informar quantos reais a pessoa vai pagar pelos dolares, considerando 6% de IOF sobre o valor em dolar
+
 package application;
+
+import util.ConversorMoeda;
+
+import java.util.Locale;
+import java.util.Scanner;
 
 public class ExFixacaoConversor {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner input = new Scanner(System.in).useLocale(Locale.US);
 
+        System.out.print("Qual o preco do dolar? ");
+        double precoDolar = input.nextDouble();
+
+        System.out.print("Quantos dolares serao comprados? ");
+        double qtdDolar = input.nextDouble();
+
+        System.out.printf("Valor a ser pago em reais = %.2f" , ConversorMoeda.valorEmReais(precoDolar, qtdDolar));
+
+        input.close();
     }
 }
--- ExerciciosSection08/src/util/ConversorMoeda.java
+++ ExerciciosSection08/src/util/ConversorMoeda.java
@@ -1,5 +1,7 @@
 package util;
 
 public class ConversorMoeda {
-
+    public static double valorEmReais(double precoDolar, double qtdDolar){
+        return (precoDolar * qtdDolar) + (precoDolar * qtdDolar * 0.06) ;
+    }
 }
