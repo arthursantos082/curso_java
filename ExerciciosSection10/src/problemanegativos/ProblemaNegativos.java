257bc673b2a4c7b467d679aef9dffd3180d53146
arthur <arthursantos0017@gmail.com>
2026-06-09 00:29:43 -0300

    Section 10 - Começando a resolver os exercícios de fixação

---
+++ ExerciciosSection10/src/problemanegativos/ProblemaNegativos.java
@@ -0,0 +1,16 @@
+//
+
+package problemanegativos;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ProblemaNegativos {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in).useLocale(Locale.US);
+        Locale.setDefault(Locale.US);
+
+
+        input.close();
+    }
+}
