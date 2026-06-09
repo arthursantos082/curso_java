c504915493de195f92833f45cf986e311d8e3f2f
arthur <arthursantos0017@gmail.com>
2026-06-09 00:24:25 -0300

    Section 10 - ProblemaExemplo02 - problema exemplo 02 - comentarios, revisao

--- ExerciciosSection10/src/problemasexemplo/ProblemaExemplo02.java
+++ ExerciciosSection10/src/problemasexemplo/ProblemaExemplo02.java
@@ -14,20 +14,23 @@ public class ProblemaExemplo02 {
         Scanner input = new Scanner(System.in).useLocale(Locale.US);
         Locale.setDefault(Locale.US);
 
+        // qtd de produtos:
         int n = input.nextInt();
+        // criacao do objeto produto com seus atributos
         Produto[] produto = new Produto[n];
 
         for (int i = 0; i < produto.length; i++) {
-            input.nextLine();
+            input.nextLine(); // \n devido ao nextInt
             String nome = input.nextLine();
             double preco = input.nextDouble();
-            // instanciar o objeto
+            // como o objeto foi criado como vetor, passamos os parametros para ele
+            // por i vezes, visto que nome e produto sao desatribuidos assim q saem do escopo
             produto[i] = new Produto(nome, preco);
         }
 
         double soma = 0.0;
         for (int i = 0; i < produto.length; i++) {
-            soma += produto[i].getPreco();
+            soma += produto[i].getPreco(); // os getters sao funcoes que retornam um valor, assim, pegamos o preco de cada item
         }
         double media = soma / produto.length;
 
