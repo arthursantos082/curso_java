de855447ef52b37773e843285f18e321d75d150e
arthur <arthursantos0017@gmail.com>
2026-06-09 00:02:43 -0300

    Section 10 - ProblemaExemplo02 - descansar? kk - getters, setters

--- ExerciciosSection10/src/problemasexemplo/ProblemaExemplo02.java
+++ ExerciciosSection10/src/problemasexemplo/ProblemaExemplo02.java
@@ -8,13 +8,14 @@ package problemasexemplo;
 import java.util.Locale;
 import java.util.Scanner;
 
-// semana de provas, vou descansar por agora... nao consigo compreender AINDA
+// descansar? se conforma voce.
 public class ProblemaExemplo02 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in).useLocale(Locale.US);
         Locale.setDefault(Locale.US);
 
         int n = input.nextInt();
+
 
 
         input.close();
