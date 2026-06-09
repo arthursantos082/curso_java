549c74ec8d3db7a74896ba8708a056a4cd758b8e
arthur <arthursantos0017@gmail.com>
2026-06-09 01:19:39 -0300

    Section 10 - ProblemaNegativos resolvido

--- ExerciciosSection10/src/problemanegativos/ProblemaNegativos.java
+++ ExerciciosSection10/src/problemanegativos/ProblemaNegativos.java
@@ -1,4 +1,8 @@
-//
+/*
+ ler um numero inteiro positivo N (maximo = 10)
+ e depois N numeros inteiros e armazena-los em um vetor
+ em seguida, mostrar na tela todos os numeros negativos lidos
+*/
 
 package problemanegativos;
 
@@ -10,6 +14,20 @@ public class ProblemaNegativos {
         Scanner input = new Scanner(System.in).useLocale(Locale.US);
         Locale.setDefault(Locale.US);
 
+        System.out.print("Quantos numeros ira digitar? ");
+        int n = input.nextInt();
+        int[] vect = new int[n];
+
+        for (int i = 0; i < vect.length; i++) {
+            input.nextLine();
+            System.out.printf("Digite o numero %d: \n", i+1);
+            vect[i] = input.nextInt();
+        }
+
+        System.out.println("NUMEROS NEGATIVOS:");
+        for (int i = 0; i < vect.length; i++) {
+            if (vect[i] < 0) System.out.println(vect[i]);
+        }
 
         input.close();
     }
