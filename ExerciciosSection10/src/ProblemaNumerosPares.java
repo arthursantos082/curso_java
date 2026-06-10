9ac03af780b9d31d9571cfad31f8d4bf7a009e67
arthur <arthursantos0017@gmail.com>
2026-06-10 00:31:32 -0300

    Section 10 - Problema Numeros Pares - resolvido

--- ExerciciosSection10/src/ProblemaNumerosPares.java
+++ ExerciciosSection10/src/ProblemaNumerosPares.java
@@ -1,7 +1,34 @@
-//
+// ler N numeros inteiros e armazena-los em um vetor
+// mostrar na tela todos os numeros pares e a qtd de numeros pares
+
+import java.util.Scanner;
 
 public class ProblemaNumerosPares {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        System.out.print("Quantos numeros ira digitar? ");
+        int n = input.nextInt();
+        int[] vetorPares = new int[n];
+        int qtdPares = 0;
+
+        for (int i = 0; i < vetorPares.length; i++) {
+            input.nextLine();
+            System.out.print("Digite um numero: ");
+            vetorPares[i] = input.nextInt();
+        }
+
+        System.out.println("\nNUMEROS PARES:");
+        for (int i = 0; i < vetorPares.length; i++) {
+            if (vetorPares[i] % 2 == 0){
+                //qtdPares += 1;
+                qtdPares++;
+                System.out.printf("%d   " , vetorPares[i]);
+            }
+        }
+
+        System.out.print("\nQUANTIDADE DE PARES = " + qtdPares);
+
+        input.close();
     }
 }
