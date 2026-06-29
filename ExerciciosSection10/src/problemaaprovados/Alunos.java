c766bb57dc13adc556d1de9705c2c570b1e3d7f3
arthur <arthursantos0017@gmail.com>
2026-06-29 10:34:50 -0300

    Section 10 - Problema Aprovados - usando OO

---
+++ ExerciciosSection10/src/problemaaprovados/Alunos.java
@@ -0,0 +1,27 @@
+package problemaaprovados;
+
+public class Alunos {
+    private String nome;
+    private double nota1;
+    private double nota2;
+
+    public Alunos(){}
+
+    public Alunos(String nome, double nota2, double nota1) {
+        this.nome = nome;
+        this.nota2 = nota2;
+        this.nota1 = nota1;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getNota1() {
+        return nota1;
+    }
+
+    public double getNota2() {
+        return nota2;
+    }
+}
---
+++ ExerciciosSection10/src/problemaaprovados/ProblemaAprovados.java
@@ -0,0 +1,34 @@
+package problemaaprovados;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ProblemaAprovados {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in).useLocale(Locale.US);
+        Locale.setDefault(Locale.US);
+
+        System.out.print("Quantos alunos serao digitados? ");
+        int n = input.nextInt();
+        Alunos[] alunos = new Alunos[n];
+
+        for (int i = 0; i < alunos.length; i++) {
+            input.nextLine();
+            System.out.printf("Digite nome, primeira e segunda nota do %do aluno: \n", (i+1) );
+            String nome = input.nextLine();
+            double nota1 = input.nextDouble();
+            double nota2 = input.nextDouble();
+
+            alunos[i] = new Alunos(nome, nota1, nota2);
+        }
+
+        System.out.println("Alunos aprovados:");
+        for (int i = 0; i < alunos.length; i++) {
+            if ((alunos[i].getNota1() + alunos[i].getNota2()) / 2 >= 6.0){
+                System.out.println(alunos[i].getNome());
+            }
+        }
+
+        input.close();
+    }
+}
